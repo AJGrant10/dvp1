e530d183cd5200599ad88311b6d7afeaff232947
AJGrant10 <ajgrant1@student.fullsail.edu>
2019-01-20 20:07:59 -0500

    Swap Name Challenge

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Welcome to My Portfolio!
 
-This repository contains coding assessments in C#.
+This repository contains coding assessments in C#. The purpose of this repository is to showcase my knowledge of C#.
+
+###Challenge Activities
+ 
---
+++ dvp.cd1/dvp1-1901-AJGrant10-master/challenges/swapname.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+using System.Reflection;
+
+namespace Dotnetheaven
+
+{
+
+    class Program
+
+    {
+
+        static void Main()
+
+        {
+
+            string name, Revname = "";
+
+            Console.Write("Entert Your Name : ");
+
+            name = Console.ReadLine();
+
+            for (int i = name.Length - 1; i >= 0; i--)
+
+            {
+
+                Revname = Revname + name[i];
+
+            }
+
+            Console.WriteLine("Reverse  String  Is  {0}", Revname);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
